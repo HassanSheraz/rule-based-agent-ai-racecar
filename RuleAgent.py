f5e2122c85792bab07474f8d86bdde199347b706
Hassan Sheraz <hsnshrz@gmail.com>
2025-06-03 17:35:07 -0500

    Added final behavior logic for open road and test rest of the parts

--- RuleAgent.py
+++ RuleAgent.py
@@ -1,6 +1,24 @@
-# Rule-based agent for Mountain Car
 class Agent:
     def chooseAction(self, observations, possibleActions):
         lidar = observations['lidar']
         velocity = observations['velocity']
-        left, s_left, center, s_right, right = lidar
+
+        left = lidar[0]
+        slight_left = lidar[1]
+        center = lidar[2]
+        slight_right = lidar[3]
+        right = lidar[4]
+
+        if center < 1.0 or slight_left < 0.8 or slight_right < 0.8:
+            return ('straight', 'brake')
+
+        if left < 1.0:
+            return ('right', 'coast')
+
+        if right < 1.0:
+            return ('left', 'coast')
+
+        if center > 2.0 and velocity < 0.8:
+            return ('straight', 'accelerate')
+
+        return ('straight', 'coast')
